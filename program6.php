6b2e1f25e93044f446665690b8a891f9f56c64ad
casetool123 <amritaghosh712000@gmail.com>
2021-07-03 12:40:04 +0530

    new

---
+++ program6.php
@@ -0,0 +1,25 @@
+<html>
+<head>
+<title>Visitor's Count</title>
+</head>
+
+<body>
+<?php
+    print "<h3> REFRESH PAGE </h3>";
+    $name="count.txt";
+
+    $file = fopen($name,"r");
+    $hits= fscanf($file,"%d");
+    fclose($file);
+
+    $hits[0]++;
+
+    $file = fopen($name,"w");
+    fprintf($file,"%d",$hits[0]);
+    fclose($file);
+
+    print "Total number of views: ".$hits[0];
+?>
+
+</body>
+</html>
